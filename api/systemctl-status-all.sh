52d331065089136087073f224ac02e51b8bc678a
Thomas Langlois <admin@adacapital.io>
2023-05-10 14:01:51 +0000

    services status script

--- api/systemctl-status-all.sh
+++ api/systemctl-status-all.sh
@@ -3,13 +3,13 @@
 if [[ $# -eq 1 && ! $1 == "" ]];then 
     PARAM=$1; 
 
-    if [[ $PARAM != "stop" && $PARAM != "start" ]];then
-        echo -e "This script expects the following parameters:\n\tUsage: $0 {start} (optional) {stop} (optional)"; 
+    if [[ $PARAM != "stop" && $PARAM != "start" && $PARAM != "stop-admin" && $PARAM != "start-admin" ]];then
+        echo -e "This script expects the following parameters:\n\tUsage: $0 {start} (optional) {start-admin} (optional) {stop} (optional) {stop-admin} (optional)"; 
         exit 2; 
     fi
 fi
 
-echo "Checking running status for CardanoBI key services:"
+echo "Checking running status for CardanoBI API key services:"
 echo
 
 STATUS=$(sudo systemctl status run.cardano-db-sync.service | head -3 | tail -1 | awk '{ print $2 }')
@@ -52,14 +52,14 @@ STATUS=$(sudo systemctl status run.cardanobi-identity-server-admin-api.service |
 if [[ $STATUS == "active" ]];then
     tput setaf 2; sudo systemctl status run.cardanobi-identity-server-admin-api.service | head -3
 
-    if [[ $PARAM == "stop" ]];then
+    if [[ $PARAM == "stop" || $PARAM == "stop-admin" ]];then
         echo "Stopping service..."
         sudo systemctl stop run.cardanobi-identity-server-admin-api.service
     fi
 else
     tput setaf 1; sudo systemctl status run.cardanobi-identity-server-admin-api.service | head -3
 
-    if [[ $PARAM == "start" ]];then
+    if [[ $PARAM == "start"|| $PARAM == "start-admin"  ]];then
         echo "Starting service..."
         sudo systemctl start run.cardanobi-identity-server-admin-api.service
     fi
@@ -71,14 +71,14 @@ STATUS=$(sudo systemctl status run.cardanobi-identity-server-admin-sts.service |
 if [[ $STATUS == "active" ]];then
     tput setaf 2; sudo systemctl status run.cardanobi-identity-server-admin-sts.service | head -3
 
-    if [[ $PARAM == "stop" ]];then
+    if [[ $PARAM == "stop" || $PARAM == "stop-admin" ]];then
         echo "Stopping service..."
         sudo systemctl stop run.cardanobi-identity-server-admin-sts.service
     fi
 else
     tput setaf 1; sudo systemctl status run.cardanobi-identity-server-admin-sts.service | head -3
 
-    if [[ $PARAM == "start" ]];then
+    if [[ $PARAM == "start" || $PARAM == "start-admin" ]];then
         echo "Starting service..."
         sudo systemctl start run.cardanobi-identity-server-admin-sts.service
     fi
@@ -89,36 +89,36 @@ STATUS=$(sudo systemctl status run.cardanobi-identity-server-admin.service | hea
 if [[ $STATUS == "active" ]];then
     tput setaf 2; sudo systemctl status run.cardanobi-identity-server-admin.service | head -3
 
-    if [[ $PARAM == "stop" ]];then
+    if [[ $PARAM == "stop" || $PARAM == "stop-admin" ]];then
         echo "Stopping service..."
         sudo systemctl stop run.cardanobi-identity-server-admin.service
     fi
 else
     tput setaf 1; sudo systemctl status run.cardanobi-identity-server-admin.service | head -3
 
-    if [[ $PARAM == "start" ]];then
+    if [[ $PARAM == "start" || $PARAM == "start-admin" ]];then
         echo "Starting service..."
         sudo systemctl start run.cardanobi-identity-server-admin.service
     fi    
 fi
 echo
 
-STATUS=$(sudo systemctl status run.cardanobi-identity-server.service | head -3 | tail -1 | awk '{ print $2 }')
-if [[ $STATUS == "active" ]];then
-    tput setaf 2; sudo systemctl status run.cardanobi-identity-server.service | head -3
+# STATUS=$(sudo systemctl status run.cardanobi-identity-server.service | head -3 | tail -1 | awk '{ print $2 }')
+# if [[ $STATUS == "active" ]];then
+#     tput setaf 2; sudo systemctl status run.cardanobi-identity-server.service | head -3
 
-    if [[ $PARAM == "stop" ]];then
-        echo "Stopping service..."
-        sudo systemctl stop run.cardanobi-identity-server.service
-    fi
-else
-    tput setaf 1; sudo systemctl status run.cardanobi-identity-server.service | head -3
+#     if [[ $PARAM == "stop" ]];then
+#         echo "Stopping service..."
+#         sudo systemctl stop run.cardanobi-identity-server.service
+#     fi
+# else
+#     tput setaf 1; sudo systemctl status run.cardanobi-identity-server.service | head -3
 
-    if [[ $PARAM == "start" ]];then
-        echo "Starting service..."
-        sudo systemctl start run.cardanobi-identity-server.service
-    fi
-fi
-echo
+#     if [[ $PARAM == "start" ]];then
+#         echo "Starting service..."
+#         sudo systemctl start run.cardanobi-identity-server.service
+#     fi
+# fi
+# echo
 
 tput sgr0
